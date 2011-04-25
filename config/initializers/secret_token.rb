979ae610def1f5b0bf7dfd2abc896fa8ef414524
userz0 <roy.kumontoy@gmail.com>
2011-04-25 20:40:19 +0700

    Initial Revision

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppZero::Application.config.secret_token = '81d3720a4b80c02e02efea363dbdf07bdf72bf7657bf9108420dea88b24a9b8217e9aca2d807c6a8d691956bcab7504d28012dda1d33eece4c3aff9fc18b9e8f'
